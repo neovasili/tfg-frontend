df2f93f5bd310ec4f7a4b1ffc9854f124932cacb
Juan Manuel Ruiz Fernández <juanmanuel@homeria.com>
2018-06-17 12:30:39 +0200

    Added several properties to properties files. Fixed other minor details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,28 +13,18 @@ import { StompConfig, StompService } from '@stomp/ng2-stompjs';
 import { environment } from '../environments/environment';
 
 const stompConfig: StompConfig = {
-  // Which server?
+
   url: environment.webSocketUrl,
 
-  // Headers
-  // Typical keys: login, passcode, host
   headers: {
-    login: 'guest',
-    passcode: 'guest'
+    login: environment.webSocketLogin,
+    passcode: environment.webSocketPass
   },
 
-  // How often to heartbeat?
-  // Interval in milliseconds, set to 0 to disable
-  heartbeat_in: 0, // Typical value 0 - disabled
-  heartbeat_out: 20000, // Typical value 20000 - every 20 seconds
-
-  // Wait in milliseconds before attempting auto reconnect
-  // Set to 0 to disable
-  // Typical value 5000 (5 seconds)
+  heartbeat_in: 0,
+  heartbeat_out: 20000,
   reconnect_delay: 5000,
-
-  // Will log diagnostics on console
-  debug: true
+  debug: environment.webSocketDebug
 };
 
 @NgModule({
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
-    production: true,
-    ticketsUrl: 'https://api-tfg.juanmanuelruizfernandez.com/',
-    webSocketUrl: 'wss://tfg-api.juanmanuelruizfernandez.com/base/websocket/'
+  production: true,
+  ticketsUrl: 'https://api-tfg.juanmanuelruizfernandez.com/',
+  webSocketUrl: 'wss://tfg-api.juanmanuelruizfernandez.com/base/websocket/',
+  webSocketLogin: 'guest',
+  webSocketPass: 'guest',
+  webSocketDebug: false,
+  apiKey: 'nWVShbRg3a4NyxxknHsg33ScUl2tsRPW6piwu9yc'
 };
